653b78c8f41baab819075dea5be152622c9b0c51
Oyedele_Musa_Funso <56215827+mfoyedele@users.noreply.github.com>
2023-07-18 11:39:34 +0100

    corrections made

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,46 +1,4 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
-<<<<<<< HEAD
-#Sets up a web server for deployment of web_static.
-
-apt-get update
-apt-get install -y nginx
-
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-	alias /data/web_static/current;
-	index index.html index.htm;
-    }
-
-    location /redirect_me {
-	return 301 http://cuberule.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-}
-
-}" > /etc/nginx/sites-available/default
-service nginx restart
-=======
-=======
->>>>>>> 72bc2d956f2bac4f0fb2c542763af45714a77199
 # sets up my web servers for the deployment of web_static
 
 echo -e "\e[1;32m START\e[0m"
@@ -88,7 +46,3 @@ echo
 #--restart NGINX
 sudo service nginx restart
 echo -e "\e[1;32m restart NGINX\e[0m"
-<<<<<<< HEAD
->>>>>>> 72bc2d956f2bac4f0fb2c542763af45714a77199
-=======
->>>>>>> 72bc2d956f2bac4f0fb2c542763af45714a77199
